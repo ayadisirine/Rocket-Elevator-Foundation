422199ee248587f058c8f68f4f03c5f3769337f5
ayadisirine <sirine.ayadi93@gmail.com>
2021-03-25 14:05:51 -0600

    linking application.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+lock "~> 3.16.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "SirineAyadi"
-set :repo_url, "https://github.com/ayadisirine/Rocket_Elevators_API.git"
+set :repo_url, "https://github.com/ayadisirine/Rocket-Elevator-Foundation.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
